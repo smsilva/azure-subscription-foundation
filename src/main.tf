120c9c1a5cc01f443d1ffa98776106a9db3c97e4
Silvio Silva <smsilva@gmail.com>
2022-02-16 16:16:19 -0300

    feat(code): create azure key vault and secret
    Create an Azure Key Vault and a Secret

--- src/main.tf
+++ src/main.tf
@@ -9,3 +9,27 @@ module "storage_account" {
   name           = local.storage_account_name
   resource_group = azurerm_resource_group.default
 }
+
+module "vault" {
+  source = "git@github.com:smsilva/azure-key-vault.git//src?ref=development"
+
+  name           = local.key_vault_name
+  resource_group = azurerm_resource_group.default
+  administrators = local.administrators
+
+  depends_on = [
+    azurerm_resource_group.default
+  ]
+}
+
+module "secret_arm_access_key" {
+  source = "git@github.com:smsilva/azure-key-vault.git//src/secret?ref=development"
+
+  vault = module.vault.instance
+  key   = "arm-access-key"
+  value = module.storage_account.instance.primary_access_key
+
+  depends_on = [
+    module.vault
+  ]
+}
--- src/outputs.tf
+++ src/outputs.tf
@@ -20,3 +20,12 @@ output "storage_account_secondary_access_key" {
   value = module.storage_account.instance.secondary_access_key
   sensitive = true
 }
+
+output "vault_id" {
+  value = module.vault.instance.id
+}
+
+output "vault_secret_arm_access_key_id" {
+  value = module.secret_arm_access_key.instance.id
+  sensitive = true
+}
