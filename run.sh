9f1b2b7e5bf4a6ecb313cede7ddc7e7ba75df4cf
Silvio Silva <smsilva@gmail.com>
2022-02-17 15:13:19 -0300

    feat(scripts): update script to retrieve backend credentials
    Update script to Retrieve Backend Credentials

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ export PATH=${PATH}:${SCRIPTS_DIRECTORY}
 export TERRAFORM_SOURCE_CODE_DIRECTORY="${SCRIPTS_DIRECTORY}/../src"
 
 create_resources && \
-retrieve_terraform_backend_credentials && \
-import_local_state_to_storage_account
+. retrieve_terraform_backend_credentials && \
+. import_local_state_to_storage_account
